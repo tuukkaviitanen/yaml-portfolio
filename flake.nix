75df6254d77f3e881f6121498a88335de86156f0
tuukkaviitanen <tuukka.viitanen@gmail.com>
2025-05-21 19:45:25 +0300

    Replaced shell.nix with flake.nix for better reproduceability

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "yaml-portfolio development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    bun-pkgs = {
+      url = "github:NixOS/nixpkgs/507b63021ada5fee621b6ca371c4fca9ca46f52c";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    bun-pkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (import bun-pkgs { inherit system; }).bun
+          ];
+        };
+      }
+    );
+}
--- shell.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  packages = with pkgs; [ 
-    (import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/507b63021ada5fee621b6ca371c4fca9ca46f52c.tar.gz") {}).bun#1.2.10
-    ];
-}
