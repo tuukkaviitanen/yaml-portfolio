35f395038a6eaae9bc9b84c2b058ed22f3b9ed01
Tuukka Viitanen <97726090+tuukkaviitanen@users.noreply.github.com>
2025-06-03 00:17:23 +0300

    Changed process.env back to Bun.env (#21)
    process.env broke NODE_ENV

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,5 @@
-export const PORT = Number(process.env.PORT) || 3000;
-export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-export const REDIS_URL = process.env.REDIS_URL;
-export const NODE_ENV = process.env.NODE_ENV;
-export const CONFIG_FILE_PATH = process.env.CONFIG_FILE_PATH || "./portfolio.yaml";
+export const PORT = Number(Bun.env.PORT) || 3000;
+export const GITHUB_TOKEN = Bun.env.GITHUB_TOKEN;
+export const REDIS_URL = Bun.env.REDIS_URL;
+export const NODE_ENV = Bun.env.NODE_ENV;
+export const CONFIG_FILE_PATH = Bun.env.CONFIG_FILE_PATH || "./portfolio.yaml";
